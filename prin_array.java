28190c80eabeca2cb84032cd063c376a3127cd68
gitdhanshri <2022bit049@sggs.ac.in>
2024-01-23 23:15:22 +0530

    Adding file to print array elements

---
+++ prin_array.java
@@ -0,0 +1,12 @@
+class prin_array{
+     public static void main(String args[]){
+	
+     int num[]={1,2,3,4,5,6,7};
+
+	System.out.println("hello");
+	for(int i=0;i<7;i++)
+	{
+		System.out.println(num[i]);
+		}
+     }
+ }
