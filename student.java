6858ead1d4a859ca75da723eddbd2ba694069c09
gitdhanshri <2022bit049@sggs.ac.in>
2024-01-27 14:31:10 +0530

    Print student rollnumAnd name

---
+++ student.java
@@ -0,0 +1,12 @@
+class student1{
+ int rollNo;
+    String studentname;
+       public static void main(String[] args){
+	student1 obj1=new student1();
+	System.out.println(obj1.rollNo);
+
+	System.out.println(obj1.studentname);
+
+}
+
+}
