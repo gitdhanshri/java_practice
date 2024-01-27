1ac8d5dcc7c479ae94217e642db4f7b123314160
gitdhanshri <2022bit049@sggs.ac.in>
2024-01-27 14:49:20 +0530

    assign value for roll num and name

--- student.java
+++ student.java
@@ -1,12 +1,20 @@
-class student1{
+ class student1{
  int rollNo;
     String studentname;
+    }
+    public class student{
        public static void main(String[] args){
 	student1 obj1=new student1();
+	obj1.rollNo=49;
+       obj1.studentname="Dhanshri";
 	System.out.println(obj1.rollNo);
-
 	System.out.println(obj1.studentname);
+	student1 obj2=new student1();
+		//obj2.rollNo=49;
+		//obj2.studentname="Dhanshri";
+	System.out.println(obj2.rollNo);
+
+	System.out.println(obj2.studentname);
 
 }
-
 }
