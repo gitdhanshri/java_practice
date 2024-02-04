9e4a9ec8d964b4566dd57b4e6f2b6984919bc758
gitdhanshri <2022bit049@sggs.ac.in>
2024-02-04 21:07:46 +0530

    Adding file which find area of circle

---
+++ area_circle.java
@@ -0,0 +1,14 @@
+class circle{
+
+	public static void main(String[] args){
+
+	double x,y;
+	double r;
+         r=10/2;
+	double c_ferrence=circumferrence(r);
+	System.out.println(c_ferrence);
+	}
+		static	double circumferrence(double x){
+			return 2*3.14*x;
+			}
+}
