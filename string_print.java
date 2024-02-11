ffa4d550d34f926713efc57de4bd057432ddcd62
gitdhanshri <2022bit049@sggs.ac.in>
2024-02-11 21:00:48 +0530

    Adding another string comparision methods

--- string_print.java
+++ string_print.java
@@ -1,20 +1,28 @@
+import java.util.*;
 class 	StringPrint{
 public static void main(String args[]){
 String s1="geeksforgeeks";
-System.out.println(s1);
+//System.out.println(s1);
 s1=s1+"microsoft";
-System.out.println(s1);
+String s2="sggs";
+/*System.out.println(s1);
 System.out.println(s1.substring(0));
 System.out.println(s1.contains("apple"));
 System.out.println(s1.contains("microsoft"));
 String s2="sggs";
-System.out.println(s2);
-System.out.println(s1.equals(s2));
-String s3="  ";
-
-for(int i=s1.length()-1	;i>=0;i--){
-s3=s3+s1[i];
+//System.out.println(s2);
+//System.out.println(s1.equals(s2));
+//System.out.println(Objects.equals(s1, s2));*/
+System.out.println("string are lexiographically equal or not-->");
+//System.out.println(s1.equals(s2));
+String name="DHANSHRI";
+String name2="dhanshri";
+System.out.println(name.equalsIgnoreCase(name2));
+if(name==name2){
+System.out.println("Equal Strings");
 }
-
+else{
+System.out.println(" Not Equal Strings");
+}
 }
 }
