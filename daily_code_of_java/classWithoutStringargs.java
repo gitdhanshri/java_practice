a6086aa32bfa5ff4647ec9a08042fd32bb6a3780
gitdhanshri <2022bit049@sggs.ac.in>
2024-03-28 10:22:29 +0530

    class Without Passing an argument to main method

---
+++ daily_code_of_java/classWithoutStringargs.java
@@ -0,0 +1,14 @@
+class Sample{
+	public static void main(String[] arr){
+		Sample s=new Sample();
+		System.out.println("Class Without String args In main");
+	}
+}
+/*
+class Sample{
+	public static void main(){
+		Sample s=new Sample();
+		System.out.println("Class Without String args In main");
+	}
+}
+*/
