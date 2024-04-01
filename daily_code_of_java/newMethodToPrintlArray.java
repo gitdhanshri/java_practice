5ff785e010a1434c28a8335e151504a0927abb09
gitdhanshri <2022bit049@sggs.ac.in>
2024-04-01 20:01:19 +0530

    new method to print an array

---
+++ daily_code_of_java/newMethodToPrintlArray.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+class ArraysFillMethod{ 
+	public static void main(String args[]){
+		ArraysFillMethod a =new ArraysFillMethod();
+		int arr1[]={11,12,23,24,25,34,36,45,56,67};
+		System.out.println(Arrays.toString(arr1));
+	}
+}
